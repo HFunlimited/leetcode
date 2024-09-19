1a044a8ed34b2dd957894ebbecb0378cb366b9bb
HF <1585751035@qq.com>
2024-09-19 16:23:11 +0800

    2024-09-19

--- src/25.k-个一组翻转链表.java
+++ src/25.k-个一组翻转链表.java
@@ -17,7 +17,33 @@
  */
 class Solution {
     public ListNode reverseKGroup(ListNode head, int k) {
+        // recursion method
 
+        ListNode curNode = head;
+        int cnt = 0;
+        while(curNode != null && cnt < k){
+            cnt ++;
+            curNode = curNode.next;
+        }
+        if(cnt < k){
+            return head;
+        }
+
+        ListNode preNode = head;
+        ListNode nextNode;
+        curNode = head.next;
+        cnt = 1;
+        while(cnt < k){
+            nextNode = curNode.next;
+            curNode.next = preNode;
+            preNode = curNode;
+            curNode = nextNode;
+            cnt++;
+        }
+        head.next = reverseKGroup(curNode, k);
+        head = preNode;
+
+        return head;
     }
 }
 // @lc code=end
